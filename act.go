bc58b973f04590cd07e504a20e8a60d8ac6f40b6
anton <anton>
2020-05-23 05:40:20 +0000

    Try now prepends caller info

--- act.go
+++ act.go
@@ -2,6 +2,8 @@ package act
 
 import (
     "fmt"
+    "path/filepath"
+    "runtime"
 )
 
 func Assert(t bool, msg string) {
@@ -19,6 +21,19 @@ func Catch(err *error) {
 
 func Try(err error) {
     if err != nil {
+        err = fmt.Errorf("%s:\n  %v", caller(2), err)
         panic(err)
     }
+}
+
+func caller(skip int) string {
+
+    pc, f, l, _ := runtime.Caller(skip)
+    dir := filepath.Base(filepath.Dir(f))
+    f    = dir + "/" + filepath.Base(f)
+    fn  := runtime.FuncForPC(pc)
+    n   := filepath.Base(fn.Name())
+
+    return fmt.Sprintf("%s[%s:%d]", n, f, l)
+
 }
